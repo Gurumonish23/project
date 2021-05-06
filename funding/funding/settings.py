af988f19e5218edc4e89f86dd2ddc1a4593c48f5
nandaanumolu <nandakishore087@gmail.com>
2021-05-06 21:05:25 +0530

    dome

--- funding/funding/settings.py
+++ funding/funding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7rw%o#e4k+dukx=%9%83qs*qg(uuu)%6qci@yqeic8c3i*8l^!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['aplyuni.herokuapp.com']
 
 
 # Application definition
