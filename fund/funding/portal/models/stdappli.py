ed6869e5ca153e916d6404a35f258e0847ede063
gurumonish <gurumonish23@gmail.com>
2021-05-06 18:54:59 +0530

    fund

---
+++ fund/funding/portal/models/stdappli.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+class Stdappli(models.Model):
+    
+    stdname= models.CharField(max_length=500,null=True)
+    univname= models.CharField(max_length=500,null=True)
+    stdmail= models.CharField(max_length=500,null=True)
+    univmail= models.CharField(max_length=500,null=True)
+    date= models.CharField(max_length=500,null=True)
+    status= models.CharField(max_length=500,null=True)
+    program=models.CharField(max_length=500,null=True)
+    agentmail=models.CharField(max_length=500,null=True)
+    def __str__(self):
+        return self.stdname
+    def register(self):
+        self.save()
+        return True
---
+++ fund/mutliforms/mutlistepforms/material-bootstrap-wizard.markdown
@@ -0,0 +1,7 @@
+Material Bootstrap Wizard
+-------------------------
+
+
+A [Pen](https://codepen.io/creativetim/pen/EgVBXa) by [Creative Tim](https://codepen.io/creativetim) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/creativetim/pen/EgVBXa/license).
