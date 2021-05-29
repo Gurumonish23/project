77d56bcaa4d7efda0fdd4937f812ae98600b47a6
gurumonish <gurumonish23@gmail.com>
2021-05-29 11:37:24 +0530

    gru

--- funding/portal/views/admin.py
+++ funding/portal/views/admin.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.shortcuts import render,redirect,HttpResponseRedirect
 from django.http import HttpResponse
 from django.views import View
+from portal.models.stddetail import Stdacd,Stdpro,Stdind,Stdcour,Stdpro1
+from portal.models.univdetail import Univdetail,Univcon
+from portal.models.studentinfo import Student
+from portal.models.universityinfo import University
+from django.contrib.auth.hashers import make_password,check_password
+from portal.models.stddetail import Stdacd,Stdcour,Stdpro,Stdpro1,Stdind
+from portal.models.stdappli import Stdappli
+from portal.models.courses import Courses
+from portal.models.application import Application
 
 def Home(request):
     return render(request,'super_admin/home.html')
@@ -10,7 +19,11 @@ def Courses(request):
 def Settings(request):
     return render(request,'super_admin/settings.html')
 def students(request):
-    return render(request,'super_admin/students.html')
+    stddetail=Stdappli.objects.all()
+    data={}
+    data['stddetail']=stddetail
+    #return render(request,'.html',data)
+    return render(request,'super_admin/students.html',data)
 def universities(request):
     return render(request,'super_admin/universities.html')
 def agent(request):
