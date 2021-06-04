fc6ef568f7637a02bfb6af929794d2d62ca1d825
nanda <nandakishore087@gmail.com>
2021-06-04 19:52:00 +0530

    settings.py

--- funding/funding/settings.py
+++ funding/funding/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7rw%o#e4k+dukx=%9%83qs*qg(uuu)%6qci@yqeic8c3i*8l^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -68,7 +68,7 @@ AUTHENTICATION_BACKENDS = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'portal/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
