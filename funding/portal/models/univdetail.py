dfc946a032ca54b6c0383c0bce2e6096a749c23b
nanda <nandakishore087@gmail.com>
2021-06-05 12:41:54 +0530

    models changed

--- funding/portal/models/univdetail.py
+++ funding/portal/models/univdetail.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 class Univdetail(models.Model):
-    Type=models.CharField(max_length=50,null=True)
-    Name=models.CharField(max_length=50,null=True)
-    Year=models.CharField(max_length=50,null=True)
-    Rank=models.CharField(max_length=50,null=True)
-    Campuses=models.CharField(max_length=50,null=True)
-    Departments = models.CharField(max_length=50,null=True)
+    Type=models.CharField(max_length=500,null=True)
+    Name=models.CharField(max_length=500,null=True)
+    Year=models.CharField(max_length=500,null=True)
+    Rank=models.CharField(max_length=500,null=True)
+    Campuses=models.CharField(max_length=500,null=True)
+    Departments = models.CharField(max_length=500,null=True)
     Email=models.EmailField()
-    Web=models.CharField(max_length=50,null=True)
-    About=models.CharField(max_length=50,null=True)
+    Web=models.CharField(max_length=500,null=True)
+    About=models.CharField(max_length=500,null=True)
     Upload=models.FileField(upload_to="",null=True)
 
     def __str__(self):
@@ -30,18 +30,18 @@ class Univdetail(models.Model):
 
 class Univcon(models.Model):
     Email=models.EmailField()
-    Intakes=models.CharField(max_length=50,null=True)
-    Award=models.CharField(max_length=50,null=True)
-    Staff=models.CharField(max_length=50,null=True)
-    Students=models.CharField(max_length=50,null=True)
-    Departments=models.CharField(max_length=50,null=True)
-    Location=models.CharField(max_length=50,null=True)
+    Intakes=models.CharField(max_length=500,null=True)
+    Award=models.CharField(max_length=500,null=True)
+    Staff=models.CharField(max_length=500,null=True)
+    Students=models.CharField(max_length=500,null=True)
+    Departments=models.CharField(max_length=500,null=True)
+    Location=models.CharField(max_length=500,null=True)
 
-    Link=models.CharField(max_length=50,null=True)
-    Face=models.CharField(max_length=50,null=True)
-    Insta=models.CharField(max_length=50,null=True)
-    Req=models.CharField(max_length=50,null=True)
-    Features=models.CharField(max_length=50,null=True)
+    Link=models.CharField(max_length=500,null=True)
+    Face=models.CharField(max_length=500,null=True)
+    Insta=models.CharField(max_length=500,null=True)
+    Req=models.CharField(max_length=500,null=True)
+    Features=models.CharField(max_length=500,null=True)
     Video=models.FileField(upload_to="",null=True)
 
     def __str__(self):
